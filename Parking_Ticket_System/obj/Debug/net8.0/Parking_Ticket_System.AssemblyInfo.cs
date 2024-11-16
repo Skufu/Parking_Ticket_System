e858ae74d0ff3bc16eb77628a441d202b1b6cc03
Skufu <agpfrancisco1@gmail.com>
2024-11-16 20:17:47 +0800

    added welcome

--- Parking_Ticket_System/obj/Debug/net8.0/Parking_Ticket_System.AssemblyInfo.cs
+++ Parking_Ticket_System/obj/Debug/net8.0/Parking_Ticket_System.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Parking_Ticket_System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fb324d2d5507ef971ab450e141521d5c3b59907")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23a62600beb23a4bab83e589d5466330602ee964")]
 [assembly: System.Reflection.AssemblyProductAttribute("Parking_Ticket_System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Parking_Ticket_System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
